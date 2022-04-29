e36b4e556f0d3e0bfc5c1a6fcb7a1172028c62e1
+Michael Kolb <+michael.w.kolb@gmail.com>
2022-04-29 13:27:20 -0400

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+
+import React, { useEffect, useState } from "react";
+import { handlers } from "../mocks/handlers";
+
+function App () {
+
+const [newData, setNewdata] = useState([]);
+const [pageLoaded, setPageLoaded] = useState(false);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setNewdata(data)
+            setPageLoaded(!pageLoaded)}
+    )}, []);
+
+    console.log(newData)
+
+    if (!pageLoaded) return <p>Loading...</p>
+
+    return (
+    <div>
+        <span><img src={newData.message} alt="A Random Dog"/></span>
+    </div>
+    )
+}
+
+export default App;
